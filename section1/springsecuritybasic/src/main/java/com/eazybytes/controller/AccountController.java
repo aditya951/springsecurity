971aba40df1245d197c30320e0f35b48df77b800
aditya <adityajatav95@gmail.com>
2024-03-04 02:34:22 +0530

    secion 3 defining users and manging

--- section1/springsecuritybasic/src/main/java/com/eazybytes/controller/AccountController.java
+++ section1/springsecuritybasic/src/main/java/com/eazybytes/controller/AccountController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AccountController {
 
+	 @GetMapping("/")
+	    public String gethome() {
+	        return "Home";
+	    }
     @GetMapping("/myAccount")
     public String getAccountDetails() {
         return "Here are the account details from the DB";
